36213e67c56968bef51b9e41fd697f53f6259043
Santiago Lezica <slezica89@gmail.com>
2012-12-07 19:42:20 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name     = 'frozendict',
+    version  = '0.1',
+    packages = ['frozendict'],
+    license  = 'MIT License',
+    long_description = open('README.txt').read()
+)
