3bdd7cec1e8ee5a1faaaf106712da7354f2c9f29
Santiago Lezica <slezica89@gmail.com>
2012-12-11 16:05:45 -0300

    Pre-package upload version

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from distutils.core import setup
 setup(
     name     = 'frozendict',
     version  = '0.1',
+
+    author       = 'Santiago Lezica',
+    author_email = 'slezica89@gmail.com',
+
     packages = ['frozendict'],
     license  = 'MIT License',
+
+    description      = 'An immutable dictionary',
     long_description = open('README.txt').read()
 )
