a08e374c4c3557b7cb8c6816497ea588468f2895
Santiago Lezica <slezica89@gmail.com>
2012-12-11 16:09:34 -0300

    Added URL for repo

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name     = 'frozendict',
     version  = '0.1',
+    url      = 'https://github.com/slezica/python-frozendict',
 
     author       = 'Santiago Lezica',
     author_email = 'slezica89@gmail.com',
