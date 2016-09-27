Marco-Sulla/python-frozendict

commit 63895ec09d5e28f4c3a308c737503c61e4e3bb43

Implement frozenordereddict

frozendict/__init__.py
@@ -2,6 +2,7 @@ import collections
 import operator
 import functools
 
+
 class frozendict(collections.Mapping):
 
     def __init__(self, *args, **kwargs):
@@ -22,6 +23,39 @@ class frozendict(collections.Mapping):
 
     def __repr__(self):
         return '<frozendict %s>' % repr(self.__dict)
+
+    def __hash__(self):
+        if self.__hash is None:
+            hashes = map(hash, self.items())
+            self.__hash = functools.reduce(operator.xor, hashes, 0)
+
+        return self.__hash
+
+
+class frozenordereddict(collections.Mapping):
+    """
+    It is an immutable wrapper around ordered dictionaries that implements the complete :py:class:`collections.Mapping`
+    interface. It can be used as a drop-in replacement for dictionaries where immutability and ordering are desired.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.__dict = collections.OrderedDict(*args, **kwargs)
+        self.__hash = None
+
+    def __getitem__(self, key):
+        return self.__dict[key]
+
+    def copy(self, **add_or_replace):
+        return frozenordereddict(self, **add_or_replace)
+
+    def __iter__(self):
+        return iter(self.__dict)
+
+    def __len__(self):
+        return len(self.__dict)
+
+    def __repr__(self):
+        return '<frozenordereddict %s>' % repr(self.__dict)
 
     def __hash__(self):
         if self.__hash is None:

commit 3181afe358dabe625b484e879132466c9ac385e7

Restore compatibility with python 2.6
63895ec09d5e28f4c3a308c737503c61e4e3bb43 accidentally broke python 2.6
compatibility due to usage of OrderedDict, which is not available
in the collections module in that version.

Possible fixes were:
1. Skip defining FrozenOrderedDict when running 2.6
2. Depend on ordereddict module when running 2.6 and import it when
needed
3. Copy OrderedDict recipe and use it when needed
4. Do not provide FrozenOrderedDict on 2.6

the last one was chosen due to maintainer preference to keep the code
base compact.

frozendict/__init__.py
@@ -1,6 +1,13 @@
 import collections
 import operator
 import functools
+import sys
+
+
+try:
+    from collections import OrderedDict
+except ImportError:  # python < 2.7
+    OrderedDict = NotImplemented
 
 
 class frozendict(collections.Mapping):
@@ -46,4 +53,8 @@ class FrozenOrderedDict(frozendict):
     A FrozenDict subclass that maintains key order
     """
 
-    dict_cls = collections.OrderedDict
+    dict_cls = OrderedDict
+
+
+if sys.version >= (2, 7):
+    del FrozenOrderedDict

setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name     = 'frozendict',
-    version  = '1.0',
+    version  = '1.1',
     url      = 'https://github.com/slezica/python-frozendict',
 
     author       = 'Santiago Lezica',