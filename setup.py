7566f29882ea42d10e46d0915f817c6c15b6de7d
Santiago Lezica <slezica89@gmail.com>
2016-10-06 10:54:50 -0300

    New version for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name     = 'frozendict',
-    version  = '1.1',
+    version  = '1.2',
     url      = 'https://github.com/slezica/python-frozendict',
 
     author       = 'Santiago Lezica',
