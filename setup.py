f58808723ab7b5e93693dfac22121c11ddb43e2c
Marco Sulla <github@marco.sulla.e4ward.com>
2020-01-26 23:15:15 +0100

    minor

--- setup.py
+++ setup.py
@@ -20,15 +20,16 @@ keywords = "immutable hashable picklable frozendict dict dictionary map Mapping 
 python_requires = ">=3.6"
 
 classifiers = (
-	"Development Status :: 5 - Production/Stable",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-	"Programming Language :: Python :: 3 :: Only",
-	"Natural Language :: English", 
-	"Operating System :: OS Independent", 
-	"Topic :: Software Development :: Libraries",
-	"Topic :: Software Development :: Libraries :: Python Modules",
-	"Topic :: Utilities", 
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6", 
+    "Natural Language :: English", 
+    "Operating System :: OS Independent", 
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities", 
 )
 
 curr_path = Path(__file__).resolve()
@@ -40,13 +41,13 @@ readme_content_type = mimetypes.guess_type(str(readme_path), strict=False)[0]
 long_description = ""
 
 with open(readme_path) as f:
-	long_description = f.read()
+    long_description = f.read()
 
 version = ""
 version_path = curr_dir / main_package_name / version_filename
 
 with open(version_path) as f:
-	version = f.read()
+    version = f.read()
 
 excluded_packages = (test_dir_name, )
 packages = setuptools.find_packages(exclude=excluded_packages)
@@ -62,19 +63,19 @@ setuptools.setup(
     license  = license,
     license_files = license_files,
     url = main_url,
-
+    
     project_urls = {
         "Bug Reports": bug_url,
         "Source": main_url,
     },
-
+    
     packages = packages,
     package_data = package_data,
-
+    
     description = description,
     long_description = long_description,
     long_description_content_type = readme_content_type,
-
+    
     classifiers = classifiers,
     keywords = keywords,
 )
