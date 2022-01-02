9785a3d173441d1592632ccb961978c3a6b3ddb5
Marco Sulla <github@marco.sulla.e4ward.com>
2022-01-02 22:27:37 +0100

    minor

--- frozendict/src/3_10/frozendictobject.c
+++ frozendict/src/3_10/frozendictobject.c
@@ -1368,7 +1368,7 @@ PyTypeObject PyFrozenDict_Type = {
     frozendict_doc,                             /* tp_doc */
     dict_traverse,                              /* tp_traverse */
     0,                                          /* tp_clear */
-    dict_richcompare,                     /* tp_richcompare */
+    dict_richcompare,                           /* tp_richcompare */
     0,                                          /* tp_weaklistoffset */
     (getiterfunc)frozendict_iter,               /* tp_iter */
     0,                                          /* tp_iternext */
@@ -1408,7 +1408,9 @@ PyTypeObject PyCoold_Type = {
     0,                                          /* tp_setattro */
     0,                                          /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC
-        | Py_TPFLAGS_BASETYPE,                  /* tp_flags */
+        | Py_TPFLAGS_BASETYPE 
+        | _Py_TPFLAGS_MATCH_SELF 
+        | Py_TPFLAGS_MAPPING,                   /* tp_flags */
     coold_doc,                                  /* tp_doc */
     dict_traverse,                              /* tp_traverse */
     0,                                          /* tp_clear */
