291e9c33e24deafd9e776f4b5333cf447a8bf90e
Marco Sulla <github@marco.sulla.e4ward.com>
2023-03-19 18:58:58 +0100

    moved monkeypatches to submodule monkeypatch

--- frozendict/__init__.py
+++ frozendict/__init__.py
@@ -1,6 +1,7 @@
 r"""
 Provides frozendict, a simple immutable dictionary.
 """
+
 try:
     from ._frozendict import *
     c_ext = True
@@ -10,96 +11,46 @@ except ImportError:
     c_ext = False
 
 from .version import version as __version__
-import collections.abc as _abc
+from . import monkeypatch
 
-# Horrible tricks - Start
 
-if c_ext:
-    import json
+def _getFrozendictJsonEncoder(BaseJsonEncoder = None):
+    if BaseJsonEncoder == None:
+        from json.encoder import JSONEncoder
+        
+        
+        BaseJsonEncoder = JSONEncoder
     
-    _OldJsonEncoder = json.encoder.JSONEncoder
-
-    class FrozendictJsonEncoder(_OldJsonEncoder):
+    
+    class FrozendictJsonEncoder(BaseJsonEncoder):
         def default(self, obj):
-            if isinstance(obj, frozendict) and not isinstance(obj, dict):
+            if isinstance(obj, frozendict):
                 # TODO create a C serializer
                 return dict(obj)
             
-            return _OldJsonEncoder.default(self, obj)
-
-    def monkeypatchJson():
-        json.JSONEncoder = FrozendictJsonEncoder
-        json.encoder.JSONEncoder = FrozendictJsonEncoder
-
-        json._default_encoder = FrozendictJsonEncoder(
-            skipkeys=False,
-            ensure_ascii=True,
-            check_circular=True,
-            allow_nan=True,
-            indent=None,
-            separators=None,
-            default=None,
-        )
-    
-    monkeypatchJson()
-    
-    def monkeypatchOrjson(oldOrjsonDumps):
-        import orjson
-
-        def myOrjsonDumps(obj, *args, **kwargs):
-            if isinstance(obj, frozendict):
-                obj = dict(obj)
             
-            return oldOrjsonDumps(obj, *args, **kwargs)
+            return BaseJsonEncoder.default(self, obj)
+    
+    
+    return FrozendictJsonEncoder
 
-        orjson.dumps = myOrjsonDumps
-        orjson.orjson.dumps = myOrjsonDumps
-    
-    try:
-        import orjson
-    except ImportError:
-        pass
-    else:
-        _oldOrjsonDumps = orjson.dumps
-        monkeypatchOrjson(_oldOrjsonDumps)
-    
-    
-    del json
-    
-else:
-    @classmethod
-    def _my_subclasshook(klass, subclass):
-        if issubclass(subclass, frozendict):
-            return False
-        
-        return NotImplemented
-    
-    _abc.MutableMapping.__subclasshook__ = _my_subclasshook
-    
-    del _my_subclasshook
 
-# Horrible tricks - End
+FrozendictJsonEncoder = _getFrozendictJsonEncoder()
+monkeypatch.patchOrUnpatchAll(patch = True, warn = False)
 
-_abc.Mapping.register(frozendict)
 
-from pathlib import Path
+from collections.abc import Mapping
+Mapping.register(frozendict)
+del Mapping
 
-curr_path = Path(__file__)
-curr_dir = curr_path.parent
 
 if c_ext:
-    __all__ = ("frozendict", )
+    __all__ = (frozendict.__name__, )
 else:
     __all__ = core.__all__
+    del core
 
 # TODO deprecated, to remove in future versions
 FrozenOrderedDict = frozendict
-__all__ += ("FrozenOrderedDict", )
 
-del Path
-del curr_path
-del curr_dir
-del _abc
-
-if not c_ext:
-    del core
+__all__ += (FrozendictJsonEncoder.__name__, "FrozenOrderedDict")
