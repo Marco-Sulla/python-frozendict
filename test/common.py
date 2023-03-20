959e252c2e359ab22eec056c4fa4cc7509159cfe
Marco Sulla <github@marco.sulla.e4ward.com>
2023-03-20 23:30:02 +0100

    also frozendict only tests are class-based now - created base.py with common attributes and fixtures

--- test/common.py
+++ test/common.py
@@ -3,6 +3,7 @@ import pickle
 from copy import copy, deepcopy
 import sys
 from collections.abc import MutableMapping
+from .base import FrozendictTestBase
 
 
 pyversion = sys.version_info
@@ -30,61 +31,7 @@ class Map(MutableMapping):
         return len(self._dict)
 
 
-class FrozendictCommonTest:
-    _frozendict_class = None
-    
-    
-    @property
-    def frozendict_class(self):
-        val = self._frozendict_class
-        
-        if val == None:
-            raise ValueError("frozendict_class is None")
-        
-        return val
-    
-    
-    @frozendict_class.setter
-    def frozendict_class(self, val):
-        self._frozendict_class = val
-    
-    
-    _c_ext = None
-    
-    
-    @property
-    def c_ext(self):
-        val = self._c_ext
-        
-        if val == None:
-            raise ValueError("c_ext is None")
-        
-        return val
-    
-    
-    @c_ext.setter
-    def c_ext(self, val):
-        self._c_ext = val
-    
-    
-    _is_subclass = None
-    
-    
-    @property
-    def is_subclass(self):
-        val = self._is_subclass
-        
-        if val == None:
-            raise ValueError("is_subclass is None")
-        
-        return val
-    
-    
-    @is_subclass.setter
-    def is_subclass(self, val):
-        self._is_subclass = val
-    
-    
+class FrozendictCommonTest(FrozendictTestBase):
     @property
     def is_mapping_implemented(self):
         return self.c_ext or (
@@ -99,91 +46,6 @@ class FrozendictCommonTest:
             (pyversion_major == 3 and pyversion_minor > 7)
         )
     
-    
-    ##########################################################################
-    # dict fixtures
-
-    @pytest.fixture
-    def fd_dict(self):
-        return {
-            "Guzzanti": "Corrado", 
-            "Hicks": "Bill", 
-            self.frozendict_class({1: 2}): "frozen"
-        }
-
-    @pytest.fixture
-    def fd_dict_hole(self, fd_dict):
-        new_dict = fd_dict.copy()
-        del new_dict["Guzzanti"]
-        return new_dict
-
-    @pytest.fixture
-    def fd_dict_eq(self):
-        return {
-            "Hicks": "Bill", 
-            "Guzzanti": "Corrado", 
-            self.frozendict_class({1: 2}): "frozen"
-        }
-
-    @pytest.fixture
-    def fd_dict_2(self):
-        return {
-            "Guzzanti": "Corrado", 
-            "Hicks": "Bill", 
-            "frozen": self.frozendict_class({1: 2})
-        }
-
-    @pytest.fixture
-    def fd_dict_sabina(self):
-        return {'Corrado': 'Guzzanti', 'Sabina': 'Guzzanti'}
-
-    @pytest.fixture
-    def generator_seq2(self, fd_dict):
-        seq2 = list(fd_dict.items())
-        seq2.append(("Guzzanti", "Mario"))
-        return (x for x in seq2)
-
-    ##########################################################################
-    # frozendict fixtures
-
-    @pytest.fixture
-    def fd(self, fd_dict):
-        return self.frozendict_class(fd_dict)
-
-    @pytest.fixture
-    def fd_hole(self, fd_dict_hole):
-        return self.frozendict_class(fd_dict_hole)
-
-    @pytest.fixture
-    def fd_unhashable(self):
-        return self.frozendict_class({1: []})
-
-    @pytest.fixture
-    def fd_eq(self, fd_dict_eq):
-        return self.frozendict_class(fd_dict_eq)
-    
-    @pytest.fixture
-    def fd2(self, fd_dict_2):
-        return self.frozendict_class(fd_dict_2)
-
-    @pytest.fixture
-    def fd_sabina(self, fd_dict_sabina):
-        return self.frozendict_class(fd_dict_sabina)
-
-    @pytest.fixture
-    def fd_items(self, fd_dict):
-        return tuple(fd_dict.items())
-
-    @pytest.fixture
-    def fd_empty(self):
-        return self.frozendict_class()
-
-    @pytest.fixture
-    def module_prefix(self):
-        if self.is_subclass:
-            return ""
-        
-        return "frozendict."
     
     ##########################################################################
     # main tests
--- test/test_frozendict.py
+++ test/test_frozendict.py
@@ -1,10 +1,11 @@
 from frozendict.core import frozendict as frozendict_class
 from .common import FrozendictCommonTest
+from .frozendict_only import FrozendictOnlyTest
 
 c_ext = False
 is_subclass = False
 
-class TestFrozendictPyCommon(FrozendictCommonTest):
+class TestFrozendictPyCommon(FrozendictCommonTest, FrozendictOnlyTest):
     frozendict_class = frozendict_class
     c_ext = c_ext
     is_subclass = is_subclass
