1ffdec4736564379aedf75658e3a9cb6c46c546b
Marco Sulla <github@marco.sulla.e4ward.com>
2023-03-20 23:55:52 +0100

    merged c tests in test_frozendict

--- test/test_frozendict.py
+++ test/test_frozendict.py
@@ -1,11 +1,20 @@
-from frozendict.core import frozendict as frozendict_class
+from frozendict.core import frozendict as frozendict_py_class
 from .common import FrozendictCommonTest
 from .frozendict_only import FrozendictOnlyTest
+import frozendict as cool
+from frozendict import frozendict as frozendict_class
 
-c_ext = False
 is_subclass = False
 
-class TestFrozendictPyCommon(FrozendictCommonTest, FrozendictOnlyTest):
-    frozendict_class = frozendict_class
-    c_ext = c_ext
-    is_subclass = is_subclass
+# class TestPyFrozendict(FrozendictCommonTest, FrozendictOnlyTest):
+#     frozendict_class = frozendict_py_class
+#     c_ext = False
+#     is_subclass = is_subclass
+
+print("SSSSSSSSSSSSSSSSSSSSS", cool.c_ext, cool)
+
+if cool.c_ext:
+    class TestCFrozendict(FrozendictCommonTest, FrozendictOnlyTest):
+        frozendict_class = frozendict_class
+        c_ext = cool.c_ext
+        is_subclass = is_subclass
