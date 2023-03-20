2155ab148fe636be12e9037d078272b5ec1fb460
Marco Sulla <github@marco.sulla.e4ward.com>
2023-03-20 20:10:54 +0100

    rewritten common tests as class tests

--- test/test_frozendict.py
+++ test/test_frozendict.py
@@ -1,18 +1,9 @@
-import pytest
 from frozendict.core import frozendict as frozendict_class
-from pathlib import Path
+from .common import FrozendictCommonTest
 
 c_ext = False
 
-curr_path = Path(__file__)
-curr_dir = curr_path.parent
 
-with open(curr_dir / "common.py") as f:
-    common_code = f.read()
-
-exec(common_code)
-
-with open(curr_dir / "frozendict_only.py") as f:
-    frozendict_only_code = f.read()
-
-exec(frozendict_only_code)
+class TestFrozendictPyCommon(FrozendictCommonTest):
+    _c_ext = c_ext
+    _frozendict_class = frozendict_class
