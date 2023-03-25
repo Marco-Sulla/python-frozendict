3ee6b6e146da6e5d4dc85c0986d8f264bf00c6fc
Marco Sulla <github@marco.sulla.e4ward.com>
2023-03-25 21:58:02 +0100

    updated test

--- test/test_freeze.py
+++ test/test_freeze.py
@@ -3,16 +3,29 @@ import frozendict as cool
 from frozendict import frozendict
 from collections import OrderedDict
 from array import array
+from types import MappingProxyType
 
 
 @pytest.fixture
 def before_cure():
-    return {"x": [5, {"y": {5, 7}}, array("B", (0, 1, 2)), OrderedDict(a=bytearray(b"a"))]}
+    return {"x": [
+        5, 
+        frozendict(y = {5, "b", memoryview(b"b")}), 
+        array("B", (0, 1, 2)), 
+        OrderedDict(a=bytearray(b"a")),
+        MappingProxyType({2: []})
+    ]}
     
 
 @pytest.fixture
 def after_cure():
-    return frozendict({'x': (5, frozendict({'y': frozenset({5, 7})}), (0, 1, 2), frozendict({'a': b'a'}))})
+    return frozendict(x = (
+        5, 
+        frozendict(y = frozenset({5, "b", memoryview(b"b")})), 
+        (0, 1, 2), 
+        frozendict(a = b'a'),
+        MappingProxyType({2: ()})
+    ))
 
 def test_deepfreeze(before_cure, after_cure):
     assert cool.deepfreeze(before_cure) == after_cure
