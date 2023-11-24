7271f468879e61c1c146d54bf5633248cc02fe4f
Marco Sulla <github@marco.sulla.e4ward.com>
2023-11-24 22:32:57 +0100

    removed pure py tests from c extension builds

--- test/test_frozendict.py
+++ test/test_frozendict.py
@@ -1,4 +1,3 @@
-from frozendict.core import frozendict as FrozendictPyClass
 from .common import FrozendictCommonTest
 from .frozendict_only import FrozendictOnlyTest
 import frozendict as cool
@@ -7,14 +6,7 @@ from frozendict import frozendict as FrozendictClass
 
 is_subclass = False
 
-
-class TestPyFrozendict(FrozendictCommonTest, FrozendictOnlyTest):
-    FrozendictClass = FrozendictPyClass
-    c_ext = False
+class TestFrozendict(FrozendictCommonTest, FrozendictOnlyTest):
+    FrozendictClass = FrozendictClass
+    c_ext = cool.c_ext
     is_subclass = is_subclass
-
-if cool.c_ext:
-    class TestCFrozendict(FrozendictCommonTest, FrozendictOnlyTest):
-        FrozendictClass = FrozendictClass
-        c_ext = cool.c_ext
-        is_subclass = is_subclass
