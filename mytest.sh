6fac176df3eed7e0026f95dd0c66c50f446d83b8
Alex Ford <a.sewall.ford@gmail.com>
2023-12-24 23:01:44 +0000

    Add typing tests to mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -1,3 +1,15 @@
 rm test/core.*
 
 ./test/debug.py && pytest
+
+if command -v mypy &> /dev/null
+then
+    pushd src
+    mypy ../test/typed.py
+    popd
+fi
+
+if command -v pyright &> /dev/null
+then
+    pyright test/typed.py
+fi
